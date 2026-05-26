6e9d7165b8fd8a169e7b379227de6b7dd821968b
Fluffybihno <huvo-tang@s.sfusd.edu>
2026-05-26 13:48:57 -0700

    Added a falling spike ball

--- spike_ball.gd
+++ spike_ball.gd
@@ -1,25 +1,28 @@
-extends Area2D
+extends Node2D
 
 @export var speed = 160.0
 var current_speed = 0.0
 
+
+func _ready() -> void:
+	current_speed = 0.0
+	
 func _physics_process(delta): 
+
 	position.y += current_speed * delta
 
-
-
-func _on_hitbox_area_entered(area: Area2D) -> void:
+func _on_hitbox_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		return
-	
-		body.take_damage(3)
-		queue_free()
-
-func _on_player_detect_area_entered(area: Area2D) -> void:
-	if area.get_parent() is Player:
-		$AnimationPlayer.play("shake")
+	body.take_damage(3)
 		
-func fall():
+func _on_player_detect_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("shake")
+	await $AnimationPlayer.animation_finished
+	_fall()
+
+func _fall():
 	current_speed = speed
 	await get_tree().create_timer(5).timeout
 	queue_free()
